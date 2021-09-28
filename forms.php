a4d1f7863be5c176da50ac907bd86f5b2249b7ab
Phill Warren <p.warren@pcs.hants.sch.uk>
2021-09-28 08:52:47 +0100

    COVID Update

--- forms.php
+++ forms.php
@@ -29,7 +29,6 @@
             <ul class="small-block-grid-1">
                 <li><a href="assets/forms/covid-consent-form.pdf" target="_blank">COVID Consent Form</a></li>
                 <li><a href="assets/forms/covid-guide.pdf" target="_blank">COVID Guide</a></li>
-                <li><a href="assets/forms/covid-checklist.pdf" target="_blank">COVID Checklist</a></li>
                 <li><a href="assets/forms/combined_consent.doc" target="_blank">Consent Form for Images, Biometrics, Internet Access and School Trips</a></li>
                 <li><a href="assets/forms/consent_withdraw.pdf" target="_blank">Consent Withdraw Form</a></li>
                 <li><a href="assets/forms/consent_form_guests.doc" target="_blank">Educational Visit Information and Consent Form Guests</a></li>
