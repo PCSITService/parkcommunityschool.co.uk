a3a4f093331ed12886041247dc6df48acfa9d479
Phill Warren <p.warren@pcs.hants.sch.uk>
2021-12-06 08:01:15 +0000

    Science update

--- science.php
+++ science.php
@@ -32,25 +32,25 @@
         <div class="large-12 columns">
         <br>
             <h3>Intent</h3>
-            <p>In science, we deliver an ambitious and broad curriculum that caters for all learners.</p>
-            <p>During KS3 in science, we want our students to be curious about the world around them and be confident to ask questions and investigate a range of possibilities.</p>
-            <p>Our curriculum is designed to sequential, building upon the learning in KS2 and enable students to develop the necessary skills when working as a scientist. Vocabulary is very important in science, and we develop the amount of science specific words learnt by students and ensure these are practised both orally and in written work.</p>
-            <p>Once at KS4, we build upon the key skills and knowledge learnt in KS3 and continue to master these and apply to a range of ideas and contexts.</p>         
+            <p>In science, we deliver an ambitious and broad curriculum that caters for all learners.
+            During Key Stage 3 in science, we want our students to be curious about the world around them and be confident to ask questions and investigate a range of possibilities.</p>
+            <p>Our curriculum is designed to sequential, building upon the learning in Key Stage 2 and enable students to develop the necessary skills when working as a scientist. Vocabulary is very important in science, and we develop the amount of science specific words learnt by students and ensure these are practised both orally and in written work.</p>
+            <p>Once at Key Stage 4, we build upon the key skills and knowledge learnt in Key Stage 3 and continue to master these and apply to a range of ideas and contexts.</p>
 
             <h3>Implementation</h3>
-            <p>We have a centralised spiral curriculum in KS3, designed to ensure NC is covered with appropriate levels of challenge for all students.</p>
+            <p>We have a centralised spiral curriculum in Key Stage 3, designed to ensure NC is covered with appropriate levels of challenge for all students.</p>
             <p>We provide opportunities for students to prepare, practise and perform, enabling them to become great learners.</p>
-            <p>For KS4, we have a centralised SOW designed to ensure AQA specification is covered from grades 1-9.</p>
-            <p>We improve and master the working scientifically skills built upon in KS3, while covering the required practical elements of the course.</p> 
+            <p>For Key Stage 4, we have a centralised SOW designed to ensure AQA specification is covered from grades 1-9.</p>
+            <p>We improve and master the working scientifically skills built upon in Key Stage 3, while covering the required practical elements of the course.</p> 
             <p>Common formative assessment tasks are used to inform both teachers and students of level of understanding and next steps.</p>
             <p>Clear focus on expanding scientific vocabulary to ensure fluency in both written and verbal answers.</p>
                      
             <br>
 
             <h3>Impact</h3>
-            <p>During KS3, we want students who ready for KS4 with necessary skills and knowledge to build upon and are ‘GCSE ready’.</p> 
-            <p>Students have high aspirations and enjoyment of science when leaving KS3 and students can achieve age related expectations (ARE) or above and are fully prepared for their GCSE.</p> 
-            <p>Once in KS4, our aim is to increase the number of students going onto study a science subject at higher education and to ensure students leave with the ability to question the changing world around them.</p>                   
+            <p>During Key Stage 3, we want students who ready for Key Stage 4 with necessary skills and knowledge to build upon and are ‘GCSE ready’.</p> 
+            <p>Students have high aspirations and enjoyment of science when leaving Key Stage 3 and students can achieve age related expectations (ARE) or above and are fully prepared for their GCSE.</p> 
+            <p>Once in Key Stage 4, our aim is to increase the number of students going onto study a science subject at higher education and to ensure students leave with the ability to question the changing world around them.</p>                   
 
             <br>
             <div class="text-center">
