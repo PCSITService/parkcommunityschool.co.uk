548c0e6e968168f53c48ff7484a855b6e6964097
Phill Warren <p.warren@pcs.hants.sch.uk>
2021-12-17 15:48:14 +0000

    Newsletter update

--- index.php
+++ index.php
@@ -236,6 +236,7 @@ END NOTIFICATION -->
             <ul class="small-block-grid-1">
                 <li>
                     <ul class="circle"><hr>
+                        <li><a href="/assets/letters/2021-12/newsletter-17-12.pdf" target="_blank">Newsletter - 17th December 2021</a></li>
                         <li><a href="/assets/letters/2021-12/newsletter-06-12.pdf" target="_blank">Newsletter - 6th December 2021</a></li>
                         <li><a href="/assets/letters/2021-11/newsletter-19-11.pdf" target="_blank">Newsletter - 19th November 2021</a></li>
                         <li><a href="/assets/letters/2021-11/newsletter-05-11.pdf" target="_blank">Newsletter - 5th November 2021</a></li>
