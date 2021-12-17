0f6da11dec3183851045dda42243b6dacc091182
Phill Warren <p.warren@pcs.hants.sch.uk>
2021-12-17 10:01:00 +0000

    Exam Information update

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
 
                 <div class="row">
                     <div class="columns">
-                        <a class="small button radius warning" href="#" data-reveal-id="exam-information">Exams</a>
+                        <a class="small button radius warning" href="#" data-reveal-id="exam-information">Year 11 Exam Information</a>
                         <a class="small button radius success" href="https://pcs.hants.sch.uk/assets/letters/2021-07/welcome-to-park.pdf" target="_blank">Welcome to Park</a>
                         <a class="small button radius warning" href="assets/prospectus.pdf" target="_blank">Prospectus</a>
                         <a class="small button radius success" href="#" data-reveal-id="newsletter">Newsletters</a>
