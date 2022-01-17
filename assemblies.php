641cba6cc4e4ec403c242dc1cdc09b143f98e282
Phill Warren <p.warren@pcs.hants.sch.uk>
2022-01-17 12:43:37 +0000

    Assemblies update

--- assemblies.php
+++ assemblies.php
@@ -36,6 +36,9 @@
                     <h5>Autumn 1</h5>
                       <a href="assets/assemblies/2021-autumn/body-and-mind.pdf" target="_blank">Body and Mind</a>
 
+                    <h5>Autumn 2</h5>
+                      <a href="assets/assemblies/2021-autumn/being-a-global-citizen.pdf" target="_blank">Being a Global Citizen</a>
+
                     <h5>Spring 1</h5>
                       <a href="assets/assemblies/2022-spring/global-citizen.pdf" target="_blank">Being a Global Citizen</a>
                     
