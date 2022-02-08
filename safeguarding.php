3e0da56adf42a7b3ac2b8e9e244be8cb570c3d52
Phill Warren <p.warren@pcs.hants.sch.uk>
2022-02-08 15:07:05 +0000

    Safegaurding update

---
+++ safeguarding.php
@@ -0,0 +1,116 @@
+<!doctype html>
+<html class="no-js" lang="en" >
+    <head>
+
+        <title>Park Community School | Safeguarding</title>
+        <?php include("includes/head.html") ?>
+    </head>
+<body class="page-parent">
+<!-- NAVIGATION -->
+<?php include("includes/topnav.html") ?>
+
+<!-- HEADER SECTION -->
+<header class="header-parent">
+    <div class="row">
+        <div class="medium-8 large-8 columns">
+            <h1>Safeguarding</h1>
+            <p class="text-capitalize"></p>
+        </div>
+    </div>
+</header>
+<!-- CONTENT -->
+<section>
+    <div class="row">
+        <br>
+        <h3>Designated Safeguarding Leads:</h3>
+            <div class="medium-4 columns text-center team-member-container">
+                <img src="images/dsl/s-bannard.jpg" alt="Lead DSL" class="team-member">
+                <p>Ms S Bannard</p>
+                <p>Lead DSL</p>
+            </div>
+
+            <div class="medium-4 columns text-center team-member-container">
+                <img src="images/dsl/j-bryce.jpg" alt="Deputy DSL" class="team-member">
+                <p>Mr J Bryce</p>
+                <p>Deputy DSL</p>
+            </div>
+
+            <div class="medium-4 columns text-center team-member-container">
+                <img src="images/dsl/s-parish.jpg" alt="Deputy DSL" class="team-member">
+                <p>Mrs S Parish</p>
+                <p>Deputy DSL</p>
+            </div>
+    </div>
+    <br><br>
+
+    <section class="grey-bg">
+    <div class="row">
+        <div class="small-centered large-1 column text-center hr-logo grey-bg"></div>
+    </div>
+    <div class="row">
+      <div class="columns">
+        <div class="row">
+          <div class="medium-6 columns">
+            <h5>Definition of Safeguarding:</h5>
+            <ul class="circle">
+              <li>Protecting children and young people (under the age of 18) from maltreatment.</li>
+              <li>Preventing impairment of children’s health or development.</li>
+              <li>Ensuring that children grow up in circumstances consistent with the provision of safe and effective care</li>
+              <li>Taking action to enable all children to have the best outcomes</li>
+             </ul>
+          </div>
+          <div class="medium-6 columns">
+            <h5>Whilst you are visiting Park you are expected to:</h5>
+            <ul class="circle">
+              <li>Sign in at reception.</li>
+              <li>Wear your red visitor badge at all times so that you are clearly identified to school staff and children</li>
+              <li>Not to be alone with students, unless you have had all relevant safeguarding checks completed, and an appropriate member of staff has approved 
+                  these checks and gives you direct permission to do so.</li>
+              <li>Report any concerns to a Designated Safeguarding Lead or, if they are unavailable, tell reception and they will seek out the relevant person to assist you.</li>
+              <li>When you leave the building, you must sign out at reception and hand in your visitor badge.</li>
+              <li>If you have concerns about a child anyone can make a direct referral to any of the following:</li>
+
+            </ul>
+            <p>Children’s Services:</p>
+            <ul>
+              <li>8.30am - 5.00pm - Tel No: 0300 555 1384</li>
+              <li>Out of Hours - Tel No: 0300 555 1373</li>
+              <li>Police - 101 or 999</li>
+              
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <br><br>
+</section>
+
+
+
+
+   
+    <br><br>
+    <?php include("includes/backtotop.html") ?>
+<!-- FOOTER -->
+<?php include("includes/footer.html") ?>
+
+
+        <!-- End Site Footer -->
+        <script src="js/vendor/libraries.min.js"></script>
+        <script src="js/vendor/foundation.min.js"></script>
+        <script src="js/app.min.js"></script>
+        <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+        <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+        <script type="text/javascript" src="slick/slick.min.js"></script>
+
+        <script type="text/javascript">
+            $(document).ready(function(){
+              $('.homepage-slider').slick({
+                autoplay: true,
+                dots: true,
+                fade: true
+              });
+            });
+        </script>
+    </body>
+</html>
