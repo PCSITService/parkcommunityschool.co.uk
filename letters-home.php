c7ee732724449fa79fb3a563aad98906bb661174
Phill Warren <p.warren@pcs.hants.sch.uk>
2022-02-11 10:10:26 +0000

    Letter update

--- letters-home.php
+++ letters-home.php
@@ -27,6 +27,7 @@
         	<ul class="small-block-grid-1">
                 <li>
                     <ul class="circle">February 2022<hr>
+                        <li><a href="/assets/letters/2022-02/review-day.pdf" target="_blank">Student Review Day</a></li>
                         <li><a href="/assets/letters/2022-02/photography-trip.pdf" target="_blank">Year 11 Photography Trip</a></li>
                     </ul>
                 <li>
