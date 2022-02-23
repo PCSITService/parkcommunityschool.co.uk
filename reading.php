a546a5a5d0726c482c327ba8f405ac8f89b310f2
Phill Warren <p.warren@pcs.hants.sch.uk>
2022-02-23 14:43:09 +0000

    Curriculum update

---
+++ reading.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html class="no-js" lang="en" >
+    <head>
+        
+        <title>Park Community School | Reading</title>
+        <?php include("includes/head.html") ?>
+    </head>
+<body class="page-learning">
+<!-- NAVIGATION -->
+<?php include("includes/topnav.html") ?>
+
+<!-- HEADER SECTION -->
+<header class="header-learning">
+    <div class="row">
+        <div class="medium-8 large-8 columns">
+            <h1>Reading</h1>
+            <p class="text-capitalize"></p>
+        </div>
+    </div>
+</header>
+<!-- Breadcrumb -->
+<div class="row">
+    <ul class="breadcrumbs" role="menubar" aria-label="breadcrumbs">
+        <li role="menuitem"><a href="/curriculum.php">Curriculum</a></li>
+        <li role="menuitem" class="current"><a href="#">Reading</a></li>
+    </ul>
+</div>
+
+<!-- CONTENT -->
+<section>
+    <div class="row">
+        <div class="large-12 columns">
+        <br>
+        <h3>Reading at Park</h3>
+        <p>Reading at Park is given status- we know it develops not only literacy, but also cultural capital, a sense of identity, empathy and compassion for others. Reading is placed at the heart of our daily work.</p> 
+        <p>Park's Reading Canon is a set of literature all students should read during their time at Park. Some of the texts will be read in tutor time together, others in English lessons, while most are promoted for independent reading.</p>
+        <p>The canon is revised regularly and incorporates a range of authors and genres designed to focus on breadth, the theme of identity, relationships and our place in society. The choices celebrate diversity , challenge discrimination and promote thinking.</p>
+        <p>Above all, the books are chosen because they are great books which support a love of reading.</p>
+
+        <!-- READING CANON -->
+            <ul class="text-center medium-block-grid-2 small-block-grid-1">
+                <h4>Reading Canon</h4>
+                <br>
+                <div class="text-center">
+                <img src="images/reading/canon.jpg" alt="Reading Canon">
+                </div>
+            </ul>
+
+        <h3>Reading within the curriculum</h3>
+        <p>Within the curriculum, texts are chosen carefully to ensure they are of good quality and provide sufficient challenge for students. Teachers draw upon a range of strategies to support students to read at an appropriately challenging level. Through exposure to challenging texts and appropriate strategies to support access to these, teaching works to extend vocabulary, improve fluency and deepen understanding of subjects.</p> 
+
+         <!-- READING PROCESS -->
+            <ul class="text-center medium-block-grid-2 small-block-grid-1">
+                <h4>Reading Process</h4>
+                <br>
+                <div class="text-center">
+                <img src="images/reading/reading-process.jpg" alt="Reading Process">
+                </div>
+            </ul>
+
+            <br>
+
+        
+        <div class="text-center">
+                        <a role="button" target="_blank" href="assets/reading/book-week.pdf" class="button">Park Book Week Monday 28 February- Friday 4 March 2022</a>
+                        <a role="button" target="_blank" href="assets/reading/read-together.pdf" class="button">Reading Together</a>
+        </div>
+
+
+    </div>
+</section>
+    
+<!-- FOOTER -->
+<?php include("includes/footer.html") ?>
+
+
+        <!-- End Site Footer -->
+        <script src="js/vendor/libraries.min.js"></script>
+        <script src="js/vendor/foundation.min.js"></script>
+        <script src="js/app.min.js"></script>
+    </body>
+</html>
