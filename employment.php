94c5b175bc58f17f1d6e187b5ec0138eeacae541
Administrator <administrator@pcs.hants.sch.uk>
2022-05-13 13:31:42 +0100

    Added HoH Maths etc advert.

--- employment.php
+++ employment.php
@@ -39,6 +39,16 @@
         </span>
       </li>-->
 
+      <li>
+        <h4>Head of House (with a preference for a qualified teacher specialising in Maths, Science or Design Technology)</h4>  
+        <p><em>Closing Date: Monday 23rd May 2022</em></p>
+
+        <span class="subheader">
+        <a href="assets/recruitment/hoh-teacher-of-maths-sci-d-t-sept-2022.pdf" target="_blank">Position Details</a>
+        </span>
+      </li>
+
+      <hr>
 
       <li>
         <h4>Teacher of Design & Technology</h4>  
@@ -50,7 +60,6 @@
       </li>
 
       <hr>
-
 
      <li>
         <h4>Cafe Assistant - Southsea</h4>  
