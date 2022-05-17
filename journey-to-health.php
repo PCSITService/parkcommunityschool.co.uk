d47f4de33fb87139cab0e5e895d037aa7ec0d057
Administrator <administrator@pcs.hants.sch.uk>
2022-05-17 15:38:45 +0100

    Added Journey to Health page under Munch More...On Site.

---
+++ journey-to-health.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html class="no-js" lang="en" >
+    <head>
+
+        <title>Park Community School | Journey to Health</title>
+        <?php include("includes/head.html") ?>
+    </head>
+<body class="page-misc">
+<!-- NAVIGATION -->
+<?php include("includes/topnav.html") ?>
+
+<!-- HEADER SECTION -->
+<header class="header-misc">
+    <div class="row">
+        <div class="medium-8 large-8 columns">
+            <h1>Journey to Health</h1>
+<!--             <p class="text-capitalize">Journey to Health</p> -->
+        </div>
+    </div>
+</header>
+
+<!-- CONTENT -->
+<!-- INTRO -->
+<section>
+    <div class="row">
+        <div class="columns">
+            <br>
+            <p>As a school we always welcome the opportunity to work with partners to benefit the students, families, and wider community.</p>
+            <p>During the Covid Pandemic through our Munch Community Food Programme we were able to provide meals and food to our community. We were in discussion with lots of organisations during this time about how the shutdown had affected individuals and we met the Social Prescribers. In April 2021 funding was provided by Government to support schools to open their facilities up after the school day. Energise Me were the distributors of the funding in our area.</p>
+            <p>This allowed us the opportunity to explore how we could make a difference to our community who were suffering with mental health problems, been shielding throughout, being overweight, loss of confidence and generally unfit.</p>
+            <p>Social Prescribers had identified a group of ladies who were in need, and we decided to provide a fitness coach and open our facilities to these ladies.</p>
+            <p>It started very slowly with lots of hand holding and encouragement. To begin with ladies walked around the site, going upstairs and downstairs and gradually over time as confidence grew and the ladies became more confident of their surroundings, they moved to the fitness suite and bigger areas for doing circuits.</p>
+            <p>The film shows this amazing Journey to Health. 
+            <p>Everyone involved are proud of the ladies’ achievements and it shows how a small amount of funding with everyone working to one goal what can be achieved.</p>
+        </div>
+    </div>
+    
+<div style="padding:75% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/710816344?h=bbfe4b300a&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Journey to Health"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+
+</section>
+
+<!-- FOOTER -->
+<?php include("includes/footer.html") ?>
+
+
+        <!-- End Site Footer -->
+        <script src="js/vendor/libraries.min.js"></script>
+        <script src="js/vendor/foundation.min.js"></script>
+        <script src="js/app.min.js"></script>
+        <script type="text/javascript" src="slick/slick.min.js"></script>
+
+        <script type="text/javascript">
+            $(document).ready(function(){
+              $('.homepage-slider').slick({
+                autoplay: true,
+                dots: true,
+                fade: true
+              });
+            });
+        </script>
+    </body>
+</html>
