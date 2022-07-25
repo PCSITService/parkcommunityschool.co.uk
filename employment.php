21d7667c74df60b146d9b08944fc623226457b72
Administrator <administrator@pcs.hants.sch.uk>
2022-07-25 17:14:03 +0100

    Remove commented ad

--- employment.php
+++ employment.php
@@ -39,15 +39,6 @@
         </span>
       </li>-->
 
-<!-- 
-     <li>
-        <h4>Cover Supervisor</h4>  
-        <p><em>Closing Date: Tuesday 12th July 2022</em></p>
-        <span class="subheader">
-        <a href="assets/recruitment/cover-supervisor-september-2022.pdf" target="_blank">Position Details</a>
-        </span>
-      </li>
- -->
 
      <li>
         <h4>Casual Assistant (Community) Weekends</h4>  
