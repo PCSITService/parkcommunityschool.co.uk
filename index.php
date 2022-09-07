9b1ae1ea1b8469ae65f099b30a1be492cc7616aa
Administrator <administrator@pcs.hants.sch.uk>
2022-09-07 09:17:09 +0100

    Remove old video from Welcome to Park... section

--- index.php
+++ index.php
@@ -351,11 +351,6 @@
 		                            <iframe src="https://player.vimeo.com/video/658514122?h=a473845737&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
                                 </div>
 		                    </div>
-                            <div>
-                                <div class="flex-video widescreen border-radius-5">
-                                    <iframe src="https://player.vimeo.com/video/586682273?h=4a3243e43b&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
-                                </div>
-                            </div>
 		                    <div>
 		                        <div class="flex-video widescreen border-radius-5">
 		                            <p>Ofsted said in 2022:</p>
