117ae0d3c0cbe06f2145e57a75df72aba5a5f89f
Administrator <administrator@pcs.hants.sch.uk>
2022-10-24 16:47:57 +0100

    Added letter to parents - Link Up Leigh Park Project.

--- letters-home.php
+++ letters-home.php
@@ -27,6 +27,7 @@
         	<ul class="small-block-grid-1">
         	    <li>
                     <ul class="circle">October 2022<hr>
+                        <li><a href="/assets/letters/2022-10/link_up_leigh_park_project.pdf" target="_blank">Link Up Leigh Park Project - Year 11</a></li>
                         <li><a href="/assets/letters/2022-10/changes_to_years_7_9_mcq_homework.pdf" target="_blank">Changes to Years 7-9 MCQ homework</a></li>
                         <li><a href="/assets/letters/2022-10/free_school_meal_price_from_31_october_2022.pdf" target="_blank">Free School Meal price from 31 October 2022</a></li>
                         <li><a href="/assets/letters/2022-10/reminder_end_of_oct_2022_half_term_and_return_31_oct_2022.pdf" target="_blank">Reminder - End of Oct 2022 Half Term and Return 31 Oct 2022</a></li>
