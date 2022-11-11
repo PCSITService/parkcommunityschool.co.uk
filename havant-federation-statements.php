b7acc500fead8176c7ab9abe867f98a149ba8ce9
Administrator <administrator@pcs.hants.sch.uk>
2022-11-11 12:58:55 +0000

    Updated vacancy. Create new sub-heading under Parent for Havant Federation Statements.

---
+++ havant-federation-statements.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html class="no-js" lang="en" >
+    <head>
+
+        <title>Park Community School | Havant Federation Statements</title>
+        <?php include("includes/head.html") ?>
+    </head>
+<body class="page-parent">
+<!-- NAVIGATION -->
+<?php include("includes/topnav.html") ?>
+
+<!-- HEADER SECTION -->
+<header class="header-parent">
+    <div class="row">
+        <div class="medium-8 large-8 columns">
+            <h1>Havant Federation Statements</h1>
+            <p class="text-capitalize"></p>
+        </div>
+    </div>
+</header>
+
+<!-- CONTENT -->
+<section>
+    <div class="row">
+        <br>
+        <div class="medium-12 columns">
+        	<ul class="small-block-grid-1">
+        	    <li>
+                    <ul class="circle">Last updated: October 2022<hr>
+                        <li><a href="/assets/havant-federation-statements/parent_letter_to_accompany_statements.pdf" target="_blank">Parent letter to accompany statements</a></li> 
+                        <li><a href="/assets/havant-federation-statements/joints_statement_mobile_phones_oct_2022.pdf" target="_blank">Joint Statement Mobile Phones Oct 2022</a></li>   
+                        <li><a href="/assets/havant-federation-statements/joint_statement_relational_conflict_and_bullying oct_2022.pdf" target="_blank">Joint Statement Relational Conflict and Bullying Oct 2022</a></li>           
+                        <li><a href="/assets/havant-federation-statements/joint_statement_standards_and_attendance_oct_2022.pdf" target="_blank">Joint Statement Standards and Attendance Oct 2022</a></li>           
+                        <li><a href="/assets/havant-federation-statements/appendix_to_attendance_statement.pdf" target="_blank">Appendix to Attendance statement</a></li>           
+                    </ul>
+                </li>           	
+
+            </ul>
+            <h5 class="subheader text-capitalize text-center"><small>Can't see what you're looking for please ask at <a href="/contact.php">the school reception</a>.</small></h5>
+        </div>
+    </div><br>
+</section>
+
+<!-- FOOTER -->
+<?php include("includes/footer.html") ?>
+
+
+        <!-- End Site Footer -->
+        <script src="js/vendor/libraries.min.js"></script>
+        <script src="js/vendor/foundation.min.js"></script>
+        <script src="js/app.min.js"></script>
+    </body>
+</html>
