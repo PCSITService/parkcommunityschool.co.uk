e04ab0032ae4d4f2c348e369614ecd4c1f39efe9
Administrator <administrator@pcs.hants.sch.uk>
2022-12-01 11:18:17 +0000

    Create page for Havant Plaza cafe.

---
+++ havant-plaza-cafe.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html class="no-js" lang="en" >
+    <head>
+
+        <title>Park Community School | The Havant Plaza Café</title>
+        <?php include("includes/head.html") ?>
+    </head>
+<body class="page-misc">
+<!-- NAVIGATION -->
+<?php include("includes/topnav.html") ?>
+
+<!-- HEADER SECTION -->
+<header class="header-misc">
+    <div class="row">
+        <div class="medium-8 large-8 columns">
+            <h1>The Havant Plaza Café</h1>
+<!--             <p class="text-capitalize">Good Times, Great Food</p> -->
+        </div>
+    </div>
+</header>
+
+<!-- CONTENT -->
+<section>
+    <div class="row">
+        <br>
+        <div class="medium-6 columns">
+            <br>
+            <p>We are pleased to work in partnership with Havant Borough Council to re-open The Plaza Café in December 2022. There is synergy between us to provide a great experience for years to come.</p>
+            <p>We offer a range of hot and cold drinks, fresh cakes made by chefs at Park Community School, a selection of hot and cold lunches, and afternoon treats. We use a range of local suppliers including Sunday Coffee Roasters, Northney Farm and Tuppeny Barn organic produce.</p>
+            <p>The menu will adapt as we learn customers requirements and as the seasons change.</p>
+            
+            
+        </div>
+
+    </div>
+    <br><br><br>
+</section>
+
+
+
+
+
+<!-- FOOTER -->
+<?php include("includes/footer.html") ?>
+
+
+        <!-- End Site Footer -->
+        <script src="js/vendor/libraries.min.js"></script>
+        <script src="js/vendor/foundation.min.js"></script>
+        <script src="js/app.min.js"></script>
+
+        <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+        <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+        <script type="text/javascript" src="slick/slick.min.js"></script>
+        <script src="https://player.vimeo.com/api/player.js"></script>
+            
+        <script type="text/javascript">
+            $(document).ready(function(){
+              $('.homepage-slider').slick({
+                autoplay: true,
+                dots: true,
+                fade: true
+              });
+            });
+        </script>
+    </body>
+</html>
