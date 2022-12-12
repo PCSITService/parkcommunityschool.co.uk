d866186cb5e00266eb666aa7da38186b4b51b1ab
Administrator <administrator@pcs.hants.sch.uk>
2022-12-12 11:35:37 +0000

    Added Year 7 Timetable Changes Jan 2023 letter.

--- letters-home.php
+++ letters-home.php
@@ -27,6 +27,7 @@
         	<ul class="small-block-grid-1">
         	    <li>
                     <ul class="circle">December 2022<hr>
+                        <li><a href="/assets/letters/2022-12/year-7-timetable-changes-january-2023.pdf" target="_blank">Year 7 - Timetable Changes January 2023</a></li>
                         <li><a href="/assets/letters/2022-12/humanities-and-art-showcase.pdf" target="_blank">Year 7 - Humanities and Art Showcase</a></li>
                         <li><a href="/assets/letters/2022-12/strep-a-infections.pdf" target="_blank">‘Strep A’ infections</a></li>
                         <li><a href="/assets/letters/2022-12/newsletter-02-12-2022.pdf" target="_blank">Newsletter - 2 December 2022</a></li>           
