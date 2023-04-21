1938f9d34a6385833e45ec7267b3d5bafc6c71c8
PCSITService <89254371+PCSITService@users.noreply.github.com>
2023-04-21 15:07:00 +0100

    Added 'WSG Manager Job Advert' - CMd

--- employment.php
+++ employment.php
@@ -39,6 +39,15 @@
         </span>
       </li>-->
   <li>
+        <h4>WSG Manager</h4>  
+        <p><em>Closing Date: Tuesday, 2nd May 2023</em></p>
+
+        <span class="subheader">
+        <a href="assets/recruitment/2023-04/wsg-manager-april-2023.pdf" target="_blank">Position Details</a>
+        </span>
+    </li>
+    <hr>   
+  <li>
         <h4>Theatre & Media Technician</h4>  
         <p><em>Closing Date: Tuesday, 25 April 2023</em></p>
 
