0ff9733601bc39c088c5c4f70fec9231306dbb73
PCSITService <89254371+PCSITService@users.noreply.github.com>
2023-04-26 15:44:04 +0100

    Added 'catering-assistant-april-2023' job advert - CMd

--- employment.php
+++ employment.php
@@ -110,6 +110,15 @@
         </span>
     </li>
     <hr>
+  <li>
+        <h4>Catering Assistant</h4>  
+        <p><em>Closing Date: Tuesday, 9th May 2023</em></p>
+
+        <span class="subheader">
+        <a href="assets/recruitment/2023-04/catering-assistant-april-2023.pdf" target="_blank">Position Details</a>
+        </span>
+    </li>
+    <hr>
 <!--         <br><span class="subheader">Please check back later.</span></ul>      -->
              
 	      </ul>
