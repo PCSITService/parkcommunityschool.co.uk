83260daf3359c82af1879525c2e15fc77c532272
PCSITService <89254371+PCSITService@users.noreply.github.com>
2023-04-28 16:02:58 +0100

    Added 'get-into-teaching.php' - CMd

---
+++ park-get-into-teaching.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html class="no-js" lang="en" >
+    <head>
+
+        <title>Park Community School | Get into Teaching</title>
+        <?php include("includes/head.html") ?>
+    </head>
+<body class="page-school">
+<!-- NAVIGATION -->
+<?php include("includes/topnav.html") ?>
+
+<!-- HEADER SECTION -->
+<header class="header-school">
+    <div class="row">
+        <div class="medium-8 large-8 columns">
+            <h1>Get into Teaching at Park</h1>
+        </div>
+    </div>
+</header>
+
+<!-- CONTENT -->
+<section>
+    <div class="row">
+        <br>
+        <div class="medium-8 columns">
+            <!--<img src="images/get-into-teaching/wildern-partnership.png" alt="Wildern Partnership"-->
+            <br><br>
+            <p>As a school we are keen to support those interested in a career in teaching, and work with Wildern Partnership SCITT and George Abbott SCITT as our local training providers.</p>
+
+            <p>Our school provides the ideal oppurtinity to undertake and complete your teaching training. We offer expert in-school support through subject mentorship complemented by a school professional themes programme. We aim to offer teaching opportunities across key stages 3 and 4, as well as experience working a tutor group allow you complete immersion into the school environment.</p>
+           
+            <p>Both Wildern Partnership and George Abbott offer quality cross phase teacher training. A School Centred Initial Teacher Training (SCITT) programme this allows you to learn the craft of teaching in a school with children and experience teachers and work as part of the team. Each trainee teacher is supported in school by an experiencd mentor along with access to dedicated teachers. Subject specialists support trainee teachers to develop their subject knowledge. Core curriculum training sessions take place in school, online and at dedicated training centres, depending on which SCITT programme you are part of.</p>
+
+            <p>To find out more about Wildern Partnership and/or George Abbott SCITT, please visit their website.</p>
+
+            <p> School experience with our school is available. Please contact Laura Firbank for further information - l.firbank@pcs.hants.sch.uk</p>
+
+            <br>
+    </div>
+                <!--<div class="small-offset-2 small-pull-2 small-8 small-8 medium-pull-0 medium-offset-0 medium-4 columns">
+                    <img src="images/schoolmeals/schoolmeals-01.jpg" alt="Our Head Chief, Hard at Work">
+                    <br><br>
+                    <a href="/assets/schoolmeals/menu.pdf" target="_blank">
+                        <img class="border-grey-radius" src="images/schoolmeals/menu.png" alt="Park Community School Kitchen - The Watering Hole Menu">
+                        <p class="text-center">Sample Menu</p>
+                    </a>
+                    <a href="/assets/schoolmeals/educatering.pdf" target="_blank">
+                        <img class="border-grey-radius" src="images/schoolmeals/educatering.jpg" alt="Educatering">
+                        <p class="text-center">Educatering - Magazine</p>-->
+                    </a>
+                    <br>
+                </div>
+            </div>
+            <br><br>
+
+
+
+
+       <!-- FOOTER -->
+        <?php include("includes/footer.html") ?>
+        <!-- End Site Footer -->
+        <script src="js/vendor/libraries.min.js"></script>
+        <script src="js/vendor/foundation.min.js"></script>
+        <script src="js/app.min.js"></script>
+    </body>
+</html>
+</html>
