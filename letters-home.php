2f67766c0ca08bab8a8a84a7b79ee39bb6435164
PCSITService <89254371+PCSITService@users.noreply.github.com>
2023-06-29 07:53:32 +0100

    Added 'Industrial Action Letter' - CMd 29.06.2023

--- letters-home.php
+++ letters-home.php
@@ -28,6 +28,7 @@
                 </li>
                  <li>
                     <ul class="circle">June 2023<hr>
+                        <li><a href="/assets/letters/2023-06/industrial-action-letter-july-2023.pdf" target="_blank">Partial School Closures on 5th July and 7th July 2023</a></li>
                         <li><a href="/assets/letters/2023-06/year-11-leavers-events-29.06.2023.pdf" target="_blank">Year 11 Leavers' Event</a></li>
                         <li><a href="/assets/letters/2023-06/fresher-week-dates-letter-3.6.2023.pdf" target="_blank">Year 6 Freshers Week amended dates</a></li>
                         <li><a href="/assets/letters/2023-06/ks3-testing-week-wc-26.03.2023.pdf" target="_blank">Year 7 - 9 Tests and Exams</a></li>
