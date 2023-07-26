8fd526564b62bfebb5e4f1109164b727edfc55c0
PCSITService <89254371+PCSITService@users.noreply.github.com>
2023-07-26 09:59:09 +0100

    Added documents and updated links to connect4families.php - CMd

--- Connect4Families.php
+++ Connect4Families.php
@@ -26,7 +26,7 @@
 			<p>Connect4Families is the creation of Park Community School staff, Susan Parish MBE and Jan Lefley MBE. In 2019 and 2020 they delivered for the Department for Education, as part of a pilot with 8 organisations, a programme of enriching activities developed for children and for parents through our Family Fun Days. As a result of the 9 successful pilots all local authorities are now funded to deliver a three-year programme of free holiday activities for disadvantaged children and families.</p>
 			<p>2023 we are delighted to be funded by Hampshire County Council, to provide 16 days of Family Fun Days and 16 days of Connect4Youth in the summer and 4 days for both at Christmas 2022. The programmes have something for everyone.</p>
            <div style="text-align: left;">
-    			<p><a href="/assets/connect4families/2023-07/activities-connect4families-summer-holidays-2023.pdf" target="_blank">Family Fun Days 2023</a>
+    			<p><a href="/assets/connect4families/2023-07/family-fun-day-poster.jpg" target="_blank">Family Fun Days 2023</a>
         		<img src="/assets/connect4families/2023-07/connect4families-logo.png" alt="Logo" style="float: right; width: 250px; margin-left: 10px;">
    				</p>
     			<p>Visit the <a href="//connect4.org.uk/" target="_blank">Connect4 website</a> for more information.</p>
@@ -40,11 +40,11 @@
 	<div class="row">
 		<div class="columns">
 			<h2>What is Connect4Youth?</h2>
-			<p>Summer 2022 we are offering a wide range of activities for 11-16years including trips out. Please see the programme below:</p>
+			<p>Summer 2023 we are offering a wide range of activities for 11-16years including trips out. Please see the programme below:</p>
 			<p>Connect4Families and Connect4Youth Activity Book is produced with lots of helpful information to give out to children and parents:
 				<div style="text-align: left;">
     			<p><a href="/assets/connect4families/2023-07/activities-connect4youth-summer-holidays-2023.pdf" target="_blank">Youth Activity Days 2023</a>
-    			<p><a href="/assets/connect4families/2023-07/activity-book.pdf" target="_blank">Youth Activity Book</a>
+    			<p><a href="/assets/connect4families/2023-07/activities-connect4families-summer-holidays-2023.pdf" target="_blank">Youth Activity Book 2023</a>
         		<img src="/assets/connect4families/2023-07/connect4youth-logo.png" alt="Logo" style="float: right; width: 250px; margin-left: 10px; ">
    				</p>
     			<p>Visit the <a href="//connect4.org.uk/" target="_blank">Connect4 website</a> for more information.</p>
