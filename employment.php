993ee41b43af7e635b3a877a8d6f4573a685cb1c
PCSITService <89254371+PCSITService@users.noreply.github.com>
2024-01-23 09:54:04 +0000

    Updated Policies Documentation

--- employment.php
+++ employment.php
@@ -45,18 +45,9 @@
         <p><em>29th January 2024</em></p>
 
         <span class="subheader">
-        <a href="assets/recruitment/2024-01/teaching-assistant-january-2024.pdf" target="_blank">Position Details</a>
-        </span>
-      </li>
-      <li>
-        <h4>Teaching Assistant</h4>  
-        <p><em>19th January 2024</em></p>
-
-        <span class="subheader">
         <a href="assets/recruitment/2024-01/science-2024.pdf" target="_blank">Position Details</a>
         </span>
       </li>
- 
 
 	      <br>
 	      <h5 class="text-center"><small>Information about School Direct visit <br><a href="//www.thesolentscitt.co.uk/" target="_blank">www.theSolentSCITT.co.uk</a></small></h5>
