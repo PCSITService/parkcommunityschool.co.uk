ee2d8336157352e09eaee1391c77fc9a7eb26007
C Maddocks <c.maddocks@pcs.hants.sch.uk>
2024-03-22 14:53:34 +0000

    Added Letters

--- letters-home.php
+++ letters-home.php
@@ -28,6 +28,8 @@
                 </li>
                  <li>
                     <ul class="circle">March 2024<hr>
+                     <li><a href="/assets/letters/2024-03/year-11-rse-session-28.03.2024.pdf" target="_blank">Year 11 Relationship & Sex Education Lesson - 28th March 2024</a></li>    
+                     <li><a href="/assets/letters/2024-03/year-9-rse-session-28.03.2024.pdf" target="_blank">Year 9 Relationship & Sex Education Lesson - 28th March 2024</a></li>     
                      <li><a href="/assets/letters/2024-03/year-9-parents-carers-options-evening-27.03.2024.pdf" target="_blank">Year 9 Parents and Carers Options Evening - 27th March 2024</a></li>    
                      <li><a href="/assets/letters/2024-03/newsletter-04.03.2024.pdf" target="_blank">Newsletter - 4th March 2024</a></li>                     
                     </ul>
