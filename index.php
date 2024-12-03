1cc6d176d07472d054ca0b14f26582dd771a9527
IT Services <itservices@pcs.hants.sch.uk>
2024-12-03 14:11:51 +0000

    Added Video to Index Page | Requested by JBY

--- index.php
+++ index.php
@@ -425,7 +425,16 @@
                                         <div style="padding:56.21% 0 0 0;position:relative;">
                                            <iframe src="https://player.vimeo.com/video/974955778?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Park Community School - Recruitment"></iframe>
                                         </div>
-                                        <script src="https://player.vimeo.com/api/player.js"></script> 
+                                        <script src="https://player.vimeo.com/api/player.js"></script>
+                                     </div> 
+                                </div>
+                                <div>
+                                    <div class="flex-video widescreen border-radius-5">
+                                        <!-- Vimeo video "Park Community School - December 2024" -->
+                                        <div style="padding:56.21% 0 0 0;position:relative;">
+                                           <iframe src="https://player.vimeo.com/video/1035604016?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Park Community School - Christmas 2024"></iframe>
+                                        </div>
+                                        <script src="https://player.vimeo.com/api/player.js"></script>  
                                     </div>
                                 </div>
                             </div>
