d4b1aad8a34dadcd4a9276f343514b2a280f9597
IT Services <itservices@pcs.hants.sch.uk>
2025-01-10 14:32:53 +0000

    Created Launchpad.php Page | Requested by CAN

---
+++ launchpad.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html class="no-js" lang="en" >
+    <head>
+
+        <title>Park Community School | Launchpad</title>
+        <?php include("includes/head.html") ?>
+    </head>
+<body class="page-school">
+<!-- NAVIGATION -->
+<?php include("includes/topnav.html") ?>
+
+<!-- HEADER SECTION -->
+<header class="header-school">
+    <div class="row">
+        <div class="medium-8 large-8 columns">
+            <h1>Launchpad</h1>
+        </div>
+    </div>
+</header>
+
+<!-- CONTENT -->
+
+<!-- FOOTER -->
+<?php include("includes/footer.html") ?>
+
+
+        <!-- End Site Footer -->
+        <script src="js/vendor/libraries.min.js"></script>
+        <script src="js/vendor/foundation.min.js"></script>
+        <script src="js/app.min.js"></script>
+    </body>
+</html>
