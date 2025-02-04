c40da0e3f9429919fc3bf58de00b1319fbce0e29
PCSITService <itservices@pcs.hants.sch.uk>
2025-02-04 07:58:23 +0000

    Updated Leave Form | Requested by SPO

--- forms.php
+++ forms.php
@@ -34,7 +34,7 @@
                 <li><a href="assets/forms/consent_form_guests.doc" target="_blank">Educational Visit Information and Consent Form Guests</a></li>
                 <li><a href="assets/forms/consent_form_students.doc" target="_blank">Educational Visit Information and Consent Form Students</a></li>
                 <li><a href="https://www.hants.gov.uk/educationandlearning/freeschoolmeals/juniorsecondary" target="_blank">Free School Meals Application Form</a></li>
-                <li><a href="assets/forms/out-round-application-inc-guidance.doc" target="_blank">Application form for admission to a Hampshire school outside the main admission round</a></li>
+                <li><a href="assets/forms/out-round-application-inc-guidance.docx" target="_blank">Application form for admission to a Hampshire school outside the main admission round</a></li>
                 <li><a href="assets/admissions/pcs_parental_combined_consent_booklet.pdf" target="_blank">Parental Combined Consent Booklet</a></li>
                 <li><a href="assets/forms/medical_consent_form.doc" target="_blank">Medical Form</a></li>
                 <li><a href="https://forms.office.com/r/TFewraG_pe" target="_blank">Consent form for Student COVID Testing</a></li>
