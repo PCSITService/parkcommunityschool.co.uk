aa536f5b2a6f1b8b76d60c564d2374adc21ac065
Christopher Maddocks <itservices@pcs.hants.sch.uk>
2025-05-11 17:41:49 +0100

    Updated Vacancy Page

--- employment.php
+++ employment.php
@@ -26,17 +26,8 @@
         <p><i>Governor vacancies are available on the <a href="governors-page.html">Governors page</a>.</i></p>
         <!--<p><i>There are currently no staff vacancies available. Please check back later for future opportunities.</i></p>-->
 
-        <h4>SEN Assistant</h4>
-        <p><a href="assets/recruitment/2025-04/SEN Assistant - Apr25.pdf" style="color: green;">Position Details</a></p>
-
-        <h4>Early Years Assistant</h4>
-        <p><a href="assets/recruitment/2025-04/Early Years Assistant Apr25.pdf" style="color: green;">Position Details</a></p>
-
-        <h4>English Teacher</h4>
-        <p><a href="assets/recruitment/2025-04/English Teacher - September 2025.pdf" style="color: green;">Position Details</a></p>
-
-        <h4>Maths Teacher</h4>
-        <p><a href="assets/recruitment/2025-04/Maths Teacher - September 2025.pdf" style="color: green;">Position Details</a></p>
+        <!--<h4>SEN Assistant</h4>
+        <p><a href="assets/recruitment/2025-04/SEN Assistant - Apr25.pdf" style="color: green;">Position Details</a></p>-->
     </div>
 
     <!-- Right Column: Documentation (Aligned to Right Edge) -->
