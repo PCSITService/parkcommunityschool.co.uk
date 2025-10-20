7b0394f8dd14b802d6a7a4dfa622fca8a3c0d38e
Christopher Maddocks <itservices@pcs.hants.sch.uk>
2025-10-20 13:39:00 +0100

    Updated Vacancy Page

--- employment.php
+++ employment.php
@@ -274,6 +274,15 @@
             <!-- Active vacancy card -->
 
             <article class="vacancy-card">
+                <h3>Teacher of Science</h3>
+                <div class="button-container">
+                    <a href="assets/recruitment/2025-10/Science -Oct 2025.pdf" class="vacancy-link" target="_blank" aria-label="Teacher of Science">
+                        <i class="fas fa-download" aria-hidden="true"></i> View Details
+                    </a>
+                </div>
+            </article>  
+
+            <article class="vacancy-card">
                 <h3>Early Years Assistant - Nursery</h3>
                 <div class="button-container">
                     <a href="assets/recruitment/2025-10/Nursery Assistant -October 2025 -2.pdf" class="vacancy-link" target="_blank" aria-label="Early Years Assistant - Nursery">
