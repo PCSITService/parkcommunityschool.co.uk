cc7f523b64e4e8eb6aec9ed57efe1bdb67eaf69b
Christopher Maddocks <itservices@pcs.hants.sch.uk>
2025-12-12 07:59:08 +0000

    Updated Job Advert - Requested by CBU

--- employment.php
+++ employment.php
@@ -280,21 +280,12 @@
                         <i class="fas fa-download" aria-hidden="true"></i> View Details
                     </a>
                 </div>
-            </article>             
-
-            <article class="vacancy-card">
-                <h3>Head of Year / Science Teacher</h3>
-                <div class="button-container">
-                    <a href="assets/recruitment/2025-11/Job Advert - Head of Year -Nov 2025.pdf" class="vacancy-link" target="_blank" aria-label="Head of Year / Science Teacher">
-                        <i class="fas fa-download" aria-hidden="true"></i> View Details
-                    </a>
-                </div>
-            </article>             
+            </article>                       
 
             <article class="vacancy-card">
                 <h3>Senior Chef de Partie</h3>
                 <div class="button-container">
-                    <a href="assets/recruitment/2025-11/Job Advert - Senior Chef De Partie -Nov 2025.pdf" class="vacancy-link" target="_blank" aria-label="Senior Chef de Partie">
+                    <a href="assets/recruitment/2025-12/Job Advert - Senior Chef De Partie -Dec 2025.pdf" class="vacancy-link" target="_blank" aria-label="Senior Chef de Partie">
                         <i class="fas fa-download" aria-hidden="true"></i> View Details
                     </a>
                 </div>
