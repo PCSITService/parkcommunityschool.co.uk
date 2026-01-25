8bfe014b9a7407e7baf7975383bd07412965def6
Christopher Maddocks <itservices@pcs.hants.sch.uk>
2026-01-25 20:27:01 +0000

    Removed IT Manager Job Vacancy

--- pages/employment.php
+++ pages/employment.php
@@ -269,15 +269,6 @@ include('../partials/header.php');
                 </a>
             </div>
         </article> 
-
-        <article class="vacancy-card">
-            <h3>IT Manager</h3>
-            <div class="button-container">
-                <a href="../assets/recruitment/2026-01/IT Manager - January 2026.pdf" class="vacancy-link" target="_blank" aria-label="IT Manager">
-                    <i class="fas fa-download" aria-hidden="true"></i> View Details
-                </a>
-            </div>
-        </article>  
         
         <!-- Governor vacancies notice -->
         <p class="text-italic">Governor vacancies are available on the <a href="governors-page.php">Governors page</a>.</p>
