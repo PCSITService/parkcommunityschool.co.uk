b7d819289b89471f989805bf124289830909c301
Josh <itservices@pcs.hants.sch.uk>
2026-02-25 15:06:22 +0000

    Updated Geog page - req s.coles

--- pages/geography.php
+++ pages/geography.php
@@ -103,7 +103,7 @@ include('../partials/header.php');
                 <h3>Intent</h3>
                 <p>Geography at Park Community School is designed to develop our students' curiosity about the physical and human world around them. Our students will investigate the local area as well as a variety of case studies from around the world. As a department we are keen to ensure that students understand how geography influences their everyday lives and how geography shapes the world around them. We also aspire to produce global thinkers; students are encouraged to think about how we are interdependent with the world around us.</p>
                 
-                <p>Our initial Year 7 curriculum focuses on the geography of our local area and how this links to our identity and heritage, igniting a sense of pride in our community. Year 7 also covers some foundations in knowledge which are then drawn upon throughout the Key Stage 3 geography journey. In Years 8 and 9 we broaden students' knowledge of human and physical geography globally, through a range of topics and specific case studies.</p>
+                <p>Our initial Year 7 curriculum in star focuses on the geography of our local area and how this links to our identity and heritage, igniting a sense of pride in our community. Year 7 also covers some foundations in knowledge which are then drawn upon throughout the Key Stage 3 geography journey. In Years 8 and 9 we broaden students' knowledge of human and physical geography globally, through a range of topics and specific case studies.</p>
                 
                 <p>Within Key Stage 3 we aim to develop geographical skills, including the ability to use a range of maps, collect and analyse data and the ability to communicate information in a variety of ways. We also aim to provide memorable fieldwork experiences allowing all year groups to see geography in action and investigate it themselves.</p>
 
@@ -114,19 +114,19 @@ include('../partials/header.php');
                 
                 <p>The geography department is ambitious for our students; teaching to the top, scaffolding so all can achieve. Reading is intrinsically linked into geography lessons with guided reading tasks in every topic linked to geographical texts. In geography we operate a 'no hands up' policy to hold the students' focus, ensuring they are ready to be called upon.</p>
                 
-                <p>In Key Stage 3 geography we use multiple choice question testing for homework where students are asked to recall knowledge from the previous lessons. The multiple choice questions are levelled to allow students to assess their progress and allow teachers to address any misconceptions. There are also termly geography assessments focusing on geographical skills. Students complete a self/peer and have a teacher assessed piece every half term. This is tracked on our geography matrix to allow students to see how they can progress.</p>
+                <p>In Key Stage 3 geography we use multiple choice question testing for homework where students are asked to recall knowledge from the previous lessons. The multiple choice questions are levelled to allow students to assess their progress and allow teachers to address any misconceptions. There are also termly geography assessments focusing on geographical skills. Students complete a self/peer and have a teacher assessed piece every half term.</p>
                 
-                <p>Each of the topics in Key Stage 3 has competencies linked to the age-related expectations. This allows students to track their progress and achieve age-related expectation in more manageable steps. To assist students in finding what they love, we have half-termly 'For the Love of' tasks, igniting the passion for the topic being studied in a range of ways. Students can find all their lessons and more on their geography student portal. This ensures students can learn from home should the situation arise or simply use it as a revision resource.</p>
+                <p>Students can find all their lessons and more on their geography student portal. This ensures students can learn from home should the situation arise or simply use it as a revision resource.</p>
 
                 <p>At GCSE we follow the Eduqas B GCSE specification which can be found online <a target="_blank" href="https://www.eduqas.co.uk/qualifications/geography-gcse-b/#tab_keydocuments">here</a>.</p>
 
                 <div class="button-group text-center">
-                    <a role="button" target="_blank" href="../assets/curriculum/geography/ks3_curriculum_map.pdf" class="button">Key Stage 3 - Learning Journey</a>
-                    <a role="button" target="_blank" href="../assets/curriculum/geography/ks4_curriculum_map.pdf" class="button">Key Stage 4 - Learning Journey</a>
+                    <a role="button" target="_blank" href="../assets/curriculum/geography/KS3 Geography overview 25 - 26.pdf" class="button">Key Stage 3 - Learning Journey</a>
+                    <a role="button" target="_blank" href="../assets/curriculum/geography/KS4 Geography Overview 2025 - 2026.pdf" class="button">Key Stage 4 - Learning Journey</a>
                 </div>
 
                 <h3>Impact</h3>
-                <p>The impact of our geography curriculum at Park is to equip our students with lifelong geographical skills and knowledge. As the future generation responsible for our planet, we want our students to understand the world and the people in it, enhanced by an understanding of both physical and human geography that makes places unique. Students will also be equipped to progress onto GCSE Geography and Further Education within geography. The Key Stage 3 students' learning is assessed against the age-related expectation bands based on the 2014 National Curriculum statements for geography.</p>
+                <p>The impact of our geography curriculum at Park is to equip our students with lifelong geographical skills and knowledge. As the future generation responsible for our planet, we want our students to understand the world and the people in it, enhanced by an understanding of both physical and human geography that makes places unique. Students will also be equipped to progress onto GCSE Geography and Further Education within geography.</p>
             </div>
         </div>
     </div>
