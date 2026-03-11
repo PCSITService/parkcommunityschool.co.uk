badc04e9360723260064b1ecdf61ec9a8e5e1009
Josh <itservices@pcs.hants.sch.uk>
2026-03-11 16:44:23 +0000

    Updated Pupil Premium doc req - e.cap

--- pages/pupil-premium.php
+++ pages/pupil-premium.php
@@ -212,7 +212,7 @@ include('../partials/header.php');
                     <li>High quality teaching which develops independence</li>
                 </ol>
 
-                <p><a href="/assets/pupilpremium/Pupil Premium Statement 2025-2028.pdf" target="_blank">Pupil Premium Strategy Document</a></p>
+                <p><a href="/assets/pupilpremium/Pupil Premium Statement 2025-2028[NEW].pdf" target="_blank">Pupil Premium Strategy Document</a></p>
 
                 <p>Our Pupil Premium strategy is based on a comprehensive understanding of our students, their families and the community. We regularly review its impact using internal and external data and current research produced by the Education Endowment Foundation. Park Community School participated in the 'Improving Outcomes for Disadvantaged Learners' project with Hampshire and Rosendale Research, and took part in the Havant Tackling Disadvantage project in partnership with other local schools and HIAS.</p>
             </div>
