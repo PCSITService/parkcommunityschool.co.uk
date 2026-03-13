96d1ae5ebb12202293f689cf5f053958b8520982
Josh <itservices@pcs.hants.sch.uk>
2026-03-13 13:48:56 +0000

    Added EHCP Cooridnator Vacacine - req d.patterson.

--- pages/employment.php
+++ pages/employment.php
@@ -262,6 +262,15 @@ include('../partials/header.php');
         <h2 class="section-header">Current Vacancies</h2>
 
         <article class="vacancy-card">
+            <h3>EHCP Coordinator</h3>
+            <div class="button-container">
+                <a href="../assets/recruitment/2026-03/EHCP Coordinator Mar26.pdf" class="vacancy-link" target="_blank" aria-label="Teacher of Science">
+                    <i class="fas fa-download" aria-hidden="true"></i> View Details
+                </a>
+            </div>
+        </article>
+
+        <article class="vacancy-card">
             <h3>Primary Teacher / Learning Support Teacher</h3>
             <div class="button-container">
                 <a href="../assets/recruitment/2026-03/Primary Teacher - Learning Support teacher - Mar 2026.pdf" class="vacancy-link" target="_blank" aria-label="Teacher of Science">
