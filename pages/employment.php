09c709e0df217db7c8e2d5201902eced8085fecc
Josh <itservices@pcs.hants.sch.uk>
2026-04-08 12:23:35 +0100

    Removed Primary Teacher vacancie req d.patterson

--- pages/employment.php
+++ pages/employment.php
@@ -269,15 +269,6 @@ include('../partials/header.php');
                 </a>
             </div>
         </article>
-
-        <article class="vacancy-card">
-            <h3>Primary Teacher / Learning Support Teacher</h3>
-            <div class="button-container">
-                <a href="../assets/recruitment/2026-03/Primary Teacher - Learning Support teacher - Mar 2026.pdf" class="vacancy-link" target="_blank" aria-label="Teacher of Science">
-                    <i class="fas fa-download" aria-hidden="true"></i> View Details
-                </a>
-            </div>
-        </article>
     
         <!-- Governor vacancies notice -->
         <p class="text-italic">Governor vacancies are available on the <a href="governors-page.php">Governors page</a>.</p>
