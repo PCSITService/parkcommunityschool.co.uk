dcd99ef1605a5fa07a02df6b85ac0327411fdf7f
Josh <itservices@pcs.hants.sch.uk>
2026-05-07 13:04:55 +0100

    Added Autumn Vacancie req c.swan

--- pages/employment.php
+++ pages/employment.php
@@ -269,6 +269,15 @@ include('../partials/header.php');
                 </a>
             </div>
         </article>
+
+        <article class="vacancy-card">
+            <h3>Café Assistant (Tea Rooms)</h3>
+            <div class="button-container">
+                <a href="../assets/recruitment/2026-04/Autumn Term Maternity Cover Teacher - KS2 - Sep. 2026.pdf" class="vacancy-link" target="_blank" aria-label="KS2 Cover Teacher">
+                    <i class="fas fa-download" aria-hidden="true"></i> View Details
+                </a>
+            </div>
+        </article>
     
         <!-- Governor vacancies notice -->
         <p class="text-italic">Governor vacancies are available on the <a href="governors-page.php">Governors page</a>.</p>
