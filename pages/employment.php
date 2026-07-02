d5b1f6d91e987b51023cb09eb730eaea868f39bf
Josh <itservices@pcs.hants.sch.uk>
2026-07-02 11:02:08 +0100

    Uploaded Vacancie req d.pat

--- pages/employment.php
+++ pages/employment.php
@@ -261,20 +261,20 @@ include('../partials/header.php');
     <section class="vacancies-column">
         <h2 class="section-header">Current Vacancies</h2>
 
-        <!--<article class="vacancy-card">
-            <h3>School Matron (Medical Officer)</h3>
+        <article class="vacancy-card">
+            <h3>Maternity Cover Teacher - KS2</h3>
             <div class="button-container">
-                <a href="../assets/recruitment/2026-06/Job Advert - School Matron - June 2026.pdf" class="vacancy-link" target="_blank" aria-label="medical Job Advert PDF">
+                <a href="../assets/recruitment/2026-07/Autumn Term Maternity Cover Teacher - KS2 - Sep. 2026 1.7.26.pdf" class="vacancy-link" target="_blank" aria-label="Maternity Cover Teacher - KS2 PDF">
                     <i class="fas fa-download" aria-hidden="true"></i> View Details
                 </a>
             </div>
-        </article> -->
+        </article>
     
         <!-- Governor vacancies notice -->
         <p class="text-italic">Governor vacancies are available on the <a href="governors-page.php">Governors page</a>.</p>
         
         <!-- Uncomment when no vacancies -->
-        <p class="text-italic">There are currently no staff vacancies available. Please check back later for future opportunities.</p>
+        <!--<p class="text-italic">There are currently no staff vacancies available. Please check back later for future opportunities.</p>-->
         
     </section>
 
