9f89cd7c7cb4a6c4979adefd44dd8fd8bedd3c03
Josh <itservices@pcs.hants.sch.uk>
2026-09-04 13:38:07 +0100

    Changed name of letter

--- pages/letters-home.php
+++ pages/letters-home.php
@@ -141,7 +141,7 @@ include('../partials/header.php');
             <div class="newsletter-item">
                 <h4>September 2026</h4>
                 <ul>
-                    <li><a href="/assets/letters/2026-09/Welcome Back 2026.pdf" target="_blank">4th September - Welcome Back 2026</a></li>
+                    <li><a href="/assets/letters/2026-09/Ofsted Report Letter to Parents and Carers.pdf" target="_blank">4th September - Ofsted Report Letter</a></li>
                     <li><a href="/assets/letters/2026-09/Year 7 trips 16  17 Sept 2026.pdf" target="_blank">Year 7 trip 16-17 September 2026</a></li>
                 </ul>
             </div>
